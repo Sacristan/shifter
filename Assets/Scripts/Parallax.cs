390f8eb176440a06daa119ca5ac3f4fbe6a1a1a7
gogeric <gogeric@localhost>
2014-01-25 23:21:36 +0200

    Waypoints and scaling background

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -7,6 +7,9 @@ public class Parallax : MonoBehaviour {
 
 	public void Start() {
 
+		Vector3 scale = transform.localScale;
+		scale.x = transform.parent.GetComponent<Chunk> ().width;
+		transform.localScale = scale;
 		Vector3 tempPos = transform.position;
 		tempPos.y = height;
 		transform.position = tempPos;
