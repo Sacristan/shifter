33acb09caf161c9de2498187ac5cdf171954cf88
gogeric <gogeric@localhost>
2014-01-25 20:51:46 +0200

    some background and jump cleanup

---
+++ Assets/Scripts/MountainSpawner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MountainSpawner : MonoBehaviour {
+
+	public GameObject[] backgrounds;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+
+	public float height;
+
+	public void Start() {
+
+		Vector3 tempPos = transform.position;
+		tempPos.y = height;
+		transform.position = tempPos;
+
+	}
+}
