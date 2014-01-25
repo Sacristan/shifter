9a63737c66d4fac7d7b01adcbd83ae13922d9425
gogeric <gogeric@localhost>
2014-01-25 15:27:39 +0200

    Update and created ChunkSpawner prefab for sake of clarity

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Chunk : MonoBehaviour {
+	public int width = 0;
+	public Material[] materials;
+	public GameObject master;
 
-	public float getWidth() {
+	public void Start() {
+		Random.seed = master.GetComponent<CubeContainer>().seed;
+		foreach (MeshRenderer MRenderer in this.GetComponentsInChildren<MeshRenderer>()) {
 
-		return 10;
+			MRenderer.material = materials[Random.Range(0,materials.Length)];
 
+		}
 	}
 }
