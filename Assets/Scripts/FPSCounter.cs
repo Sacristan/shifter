20428907155929f233d4541e887fa54701eb7003
Sac <Sac@SacPC>
2014-01-26 00:32:12 +0200

    Added WayPointPosUpdateControllers + some AI stuff

---
+++ Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSCounter : MonoBehaviour
+{
+    private float fps;
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width - 25, 0, 50, 50), fps.ToString());
+    }
+
+    void Update()
+    {
+        fps = Mathf.RoundToInt((1.0f / Time.smoothDeltaTime) * 100) / 100;
+    }
+}
