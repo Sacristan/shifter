31379cdd81fad473ead7ec21717393d810b55604
gogeric <gogeric@localhost>
2014-01-26 01:29:10 +0200

    COrrected waypoints

--- Assets/Scripts/CharacterAnimation.cs
+++ Assets/Scripts/CharacterAnimation.cs
@@ -5,29 +5,29 @@ public class CharacterAnimation : MonoBehaviour {
 
 	public Texture2D[] idle;
 	public Texture2D[] running;
-	private float runningCurrentTexture;
-
+	private float runningCurrentTexture = 0F;
+	private int runningCurrentTextureInt = 0;
 	public Texture2D[] jumping;
 	private int jumpingCurrentTexture;
 
+	//private Rigidbody rigidbody;
 	private Material material;
 
 	public void Awake() {
-
 		material = this.GetComponent<MeshRenderer> ().material;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		StartCoroutine (Running ());
+		StartCoroutine(Running ());
 	}
 
 	public IEnumerator Running() {
-
-		material.mainTexture = running [Mathf.FloorToInt(runningCurrentTexture)];
-		runningCurrentTexture = Mathf.PingPong(runningCurrentTexture, running.Length);
-
-		yield return new WaitForSeconds(0.01F);
+		runningCurrentTexture += (float)1/running.Length;
+		material.mainTexture = running[runningCurrentTextureInt];
+		runningCurrentTextureInt = Mathf.FloorToInt(Mathf.PingPong(runningCurrentTexture, running.Length));
+		
+		yield return new WaitForSeconds(0.1F);
 	}
 
 	public IEnumerator Jumping() {
