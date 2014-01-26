c52ba6c3c83484a394dede91dbee929facdff455
gogeric <gogeric@localhost>
2014-01-26 02:53:39 +0200

    running right and left idle animation basic animator logic

--- Assets/Scripts/CharacterAnimation.cs
+++ Assets/Scripts/CharacterAnimation.cs
@@ -3,37 +3,15 @@ using System.Collections;
 
 public class CharacterAnimation : MonoBehaviour {
 
-	public Texture2D[] idle;
-	public Texture2D[] running;
-	private float runningCurrentTexture = 0F;
-	private int runningCurrentTextureInt = 0;
-	public Texture2D[] jumping;
-	private int jumpingCurrentTexture;
-
-	//private Rigidbody rigidbody;
-	private Material material;
+	private Animator animator;
 
 	public void Awake() {
-		material = this.GetComponent<MeshRenderer> ().material;
+		animator = this.GetComponent<Animator> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		StartCoroutine(Running ());
-	}
-
-	public IEnumerator Running() {
-		runningCurrentTexture += (float)1/running.Length;
-		material.mainTexture = running[runningCurrentTextureInt];
-		runningCurrentTextureInt = Mathf.FloorToInt(Mathf.PingPong(runningCurrentTexture, running.Length));
-		
-		yield return new WaitForSeconds(0.1F);
-	}
-
-	public IEnumerator Jumping() {
-		
-		
-		yield return new WaitForSeconds(0.01F);
+		animator.SetFloat("speedx", this.rigidbody.velocity.x);
 	}
 
 }
