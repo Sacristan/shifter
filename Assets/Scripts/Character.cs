bdd71eee00cf99bc6ab51712036634747fe3f546
gogeric <gogeric@localhost>
2014-01-26 10:35:54 +0200

    Corrected and added chunks added char physic material

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -11,29 +11,33 @@ public class Character : MonoBehaviour {
 	private Vector2 move = Vector2.zero;
 	private Rigidbody rig2D;
 	private int jumpCount = 0;
+	private float previousVelocity = 0;
 
-	// Use this for initialization
+	private Ray leftRay;
+	private RaycastHit hitLeft;
+	private Ray rightRay;
+	private RaycastHit hitRight;
+
 	void Awake () {
 		rig2D = this.GetComponent<Rigidbody> ();
 		correctedSpeed = speed * 100;
 
+
+		hitLeft = new RaycastHit();
+
+		hitRight = new RaycastHit();
+
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 
-
-
-		pos = transform.position;
-		//pos.x += Input.GetAxis ("Horizontal")*Time.deltaTime * speed;
-
-		pos.z = 0;
-		transform.position = pos;
 		move = new Vector2 (Input.GetAxis("Horizontal")*Time.deltaTime*correctedSpeed,(Jump() ? jumpStrength:0));
+		//move.x *= detectCloseWalls()*10F;
 		rig2D.AddForce(move);
 		Vector3 velocity = rig2D.velocity;
 		velocity.x = Mathf.Clamp (rig2D.velocity.x,-10F, 10F);
 		rig2D.velocity = velocity;
+
 	}
 
 	private bool Jump() {
@@ -46,10 +50,40 @@ public class Character : MonoBehaviour {
 		}
 	}
 
+	public void LateUpdate(){
+
+		previousVelocity = rigidbody.velocity.x;
+
+	}
+
 	void OnCollisionStay(Collision collision) {
 		jumpCount = 0;
 	}
 	void OnCollisionEnter(Collision collision) {
 		jumpCount = 0;
 	}
+
+	private bool detectCloseWalls() {
+		bool left = false;
+		bool right = false;
+		Vector3 rayStart = transform.position + Vector3.up;
+		leftRay = new Ray (rayStart, Vector3.left);
+		Debug.DrawRay (rayStart, Vector3.left);
+		rightRay = new Ray (rayStart, Vector3.right);
+		Debug.DrawRay (rayStart, Vector3.right);
+
+		if (collider.Raycast (leftRay, out hitLeft, 12)) {
+			if(Mathf.Abs(Vector3.Distance(this.transform.position,hitLeft.point)) < 0.5F){
+				left = true;
+			}
+		}
+		if (collider.Raycast (rightRay, out hitRight, 12)) {
+			if(Mathf.Abs(Vector3.Distance(this.transform.position,hitRight.point)) < 0.5F){
+				right = true;
+			}
+		}
+		return left || right;
+
+	}
+	
 }
