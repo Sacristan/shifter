08eabd1f9acce27c203a758cebd383a50be64eab
gogeric <gogeric@localhost>
2014-01-26 11:25:44 +0200

    some more

---
+++ Assets/Scripts/flicker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flicker : MonoBehaviour {
+
+	private Light light;
+	// Use this for initialization
+	void Start () {
+		light = this.GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		light.intensity = Random.Range (1.0F, 2.0F);
+	}
+}
