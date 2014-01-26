f491b50ada7c893ba6cb334bb94a7efd7bc39807
gogeric <gogeric@localhost>
2014-01-26 15:08:52 +0200

    final?

--- Assets/Scripts/CharacterAnimation.cs
+++ Assets/Scripts/CharacterAnimation.cs
@@ -4,15 +4,32 @@ using System.Collections;
 public class CharacterAnimation : MonoBehaviour {
 
 	private Animator animator;
+	private AudioSource ASource;
+
+	public AudioClip walk;
+
+	public AudioClip[] Jumps;
 
 	public void Awake() {
 		animator = this.GetComponent<Animator> ();
+		ASource = this.GetComponent<AudioSource> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 		animator.SetFloat("speedx", this.rigidbody.velocity.x);
 		animator.SetFloat("speedy", this.rigidbody.velocity.y);
+		Vector3 velocity = this.rigidbody.velocity;
+		if (Mathf.Abs(velocity.x)>0.1 &&Mathf.Abs(velocity.y)<0.1  && !ASource.audio.isPlaying) {
+			ASource.audio.Play();
+		}
+		if ((Mathf.Abs(velocity.x)<0.1 || Mathf.Abs(velocity.y)>0.1)  && ASource.audio.isPlaying) {
+			ASource.audio.Stop();
+		}
+		if (Input.GetButtonDown("Jump")) {
+			//ASource.audio.clip = Jumps[Random.Range(0,Jumps.Length)];
+			ASource.audio.PlayOneShot(Jumps[Random.Range(0,Jumps.Length)]);
+		}
 	}
 
 }
--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -5,17 +5,40 @@ using System.Collections.Generic;
 public class Chunk : MonoBehaviour {
 	public int width = 0;
 	private Material[] materials;
+	
+	private PhaseElements phase;
 	//public GameObject master;
-
+	int getBGLayer;
 	public void Start() {
-		int getBGLayer = LayerMask.NameToLayer ("Background"); //TODO:set that to "Cubes" to be exclusive instead of inclusive
-		Random.seed = ChunkSpawner.currentPhase.seed;
-		materials = ChunkSpawner.currentPhase.terrainMaterials;
+		phase = ChunkSpawner.currentPhase;
+		getBGLayer = LayerMask.NameToLayer ("Background"); //TODO:set that to "Cubes" to be exclusive instead of inclusive
+		Random.seed = phase.seed;
+		materials = phase.terrainMaterials;
 		foreach (MeshRenderer MRenderer in this.GetComponentsInChildren<MeshRenderer>()) {
 			if(MRenderer.gameObject.layer != getBGLayer) {
 				MRenderer.material = materials[Random.Range(0,materials.Length)];
 			}
 		}
+	}
+
+	public void Update() {
+
+		if (phase != ChunkSpawner.currentPhase) {
+
+			SwitchPhase(ChunkSpawner.currentPhase);
+
+		}
+
+	}
+
+	public void SwitchPhase(PhaseElements _Phase) {
+		materials = _Phase.terrainMaterials;
+		foreach (MeshRenderer MRenderer in this.GetComponentsInChildren<MeshRenderer>()) {
+			if(MRenderer.gameObject.layer != getBGLayer) {
+				MRenderer.material = materials[Random.Range(0,materials.Length)];
+			}
+		}
+		this.phase = _Phase;
 	}
 
 }
